43d2df2da3659616be6d2b3ff5ee8a2798a85d99
Pyunghyuk Yoo <yoophi@utonium.local>
2021-09-09 01:27:44 +0900

    FEAT: add tailwindcss, bootstrap, reactstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="m-4 text-blue-500 font-bold">
+      <p class="font-bold text-2xl">hello, world</p>
+      <Button color="danger" className="border border-gray-200">
+        Dannger!
+      </Button>
     </div>
   );
 }
